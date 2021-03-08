7d6ed8c1b752eb1da5cb4f0b881eb0250882b6a4
jamie tardi <66372574+jamieTardi@users.noreply.github.com>
2021-03-08 17:02:19 +0000

    initial commit

---
+++ todoListRazor/Models/Todo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todoListRazor.Models
+{
+    public class Todo
+    {
+        //This is the database key
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string TodoItem { get; set; }
+
+    }
+}
