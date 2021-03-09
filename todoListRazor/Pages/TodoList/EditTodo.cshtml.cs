bbb0d62baa3e9a3070bb785dc8766ae045c84073
jamie tardi <66372574+jamieTardi@users.noreply.github.com>
2021-03-09 12:20:47 +0000

    Edit function now works as intended.

--- todoListRazor/Pages/TodoList/EditTodo.cshtml.cs
+++ todoListRazor/Pages/TodoList/EditTodo.cshtml.cs
@@ -4,13 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using todoListRazor.Models;
 
 namespace todoListRazor.Pages.TodoList
 {
     public class EditTodoModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+
+        public EditTodoModel(ApplicationDbContext db)
         {
+            _db = db;
         }
+
+        [BindProperty]
+        public Todo Todo { get; set; }
+        public async Task OnGet(int id)
+        {
+            Todo = await _db.Todo.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+
+            var TodoFromDb = await _db.Todo.FindAsync(Todo.Id);
+            TodoFromDb.TodoItem = Todo.TodoItem;
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+
+        }
+      
     }
 }
