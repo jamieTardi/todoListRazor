89375b4c4d26c0f66cf330efcda5252b8415e4bd
jamie tardi <66372574+jamieTardi@users.noreply.github.com>
2021-03-09 09:42:20 +0000

    Adding the table for the todo List

--- todoListRazor/Pages/TodoList/Index.cshtml.cs
+++ todoListRazor/Pages/TodoList/Index.cshtml.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using todoListRazor.Models;
 
 namespace todoListRazor.Pages.TodoList
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        //Looping over the todos
+        public IEnumerable<Todo> Todos { get; set; }
+        public async Task OnGet()
+        {
+            Todos = await _db.Todo.ToListAsync();
         }
     }
 }
